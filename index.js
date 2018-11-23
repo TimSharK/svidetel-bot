987ce1173b5f10052aaa7d9057d576d0b0fdb1b0
Raydzi <45210724+Raydzi@users.noreply.github.com>
2018-11-24 03:56:54 +0500

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ require('https').createServer().listen(process.env.PORT || 5000).on('request', f
 
 var scene1Greet = 'Здравствуйте! Насколько я понимаю вы решили заняться этим загадочным происшествием. Что ж проходите. Это моё детище "древние инсталляции". Как жаль, что лучшая экспозиция была испорчена. Вы можете здесь свободно ходить и задавать любые вопросы. Одна из краж случилась на втором этаже. Был украден самый большой фаллос в мире. Всего там их было 3. Осмотрите место преступления и сообщите мне если что-то найдёте.';
 var scene2Greet = 'Чудесно. Вы говорите, что след ведёт в зал с музыкальными инструментами? Там была украдена золотая Арфа.';
+var scene3Greet = 'Cтранно, что пропала именно она. Ведь эта арфа принадлежала Малевичу. Его картина Чёрный квадрат также висит в моем музее.';
+var scene4Greet = 'Под картиной вы нашли записку? Так так. "встретимся под самой тёмной лестницей" ';
+var scene5Greet = 'Вы обнаружили носовой платок, с буквами А.С.? Инициалы прям как у Пушкина.';
+
+
 
 var scene1 = new Scene("scene1");
 scene1.enter((ctx) => ctx.reply(scene1Greet));
@@ -30,10 +35,41 @@ scene2.on('message', (ctx) => {
         ctx.reply("Что-то пошло не так");
 });
 
+var scene3 = new Scene("scene3");
+scene3.enter((ctx) => ctx.reply(scene3Greet));
+scene3.on('message', (ctx) => {
+    if (ctx.message.text === "5dr643")
+        ctx.scene.enter("scene3");
+    else
+        ctx.reply("Что-то пошло не так");
+});
+
+var scene4 = new Scene("scene4");
+scene4.enter((ctx) => ctx.reply(scene4Greet));
+scene3.on('message', (ctx) => {
+    if (ctx.message.text === "3d3r43")
+        ctx.scene.enter("scene4");
+    else
+        ctx.reply("Что-то пошло не так");
+});
+
+var scene5 = new Scene("scene5");
+scene5.enter((ctx) => ctx.reply(scene5Greet));
+scene5.on('message', (ctx) => {
+    if (ctx.message.text === "6d76r4")
+        ctx.scene.enter("scene5");
+    else
+        ctx.reply("Что-то пошло не так");
+});
+
+
+
 const stage = new Stage();
 stage.register(scene1);
 stage.register(scene2);
-
+stage.register(scene3);
+stage.register(scene4);
+stage.register(scene5);
 bot.use(session());
 bot.use(stage);
 
